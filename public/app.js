b154fc0866f31df2b94f44ba84bfc6686fa76687
Graeme Brown <graemehbrown@googlemail.com>
2018-02-19 22:34:26 +0000

    Task 3 complete.  Removed the data in the HTML and populated with data from an array of objects.  Some small styling changes made in CSS.

--- public/app.js
+++ public/app.js
@@ -1,6 +1,34 @@
-const app = function () {
+const catArray = [
+    {
+        name: 'Boba',
+        favouriteFood: 'sock fluff',
+        image: 'http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg'
+    },
+    {
+        name: 'Barnaby',
+        favouriteFood: 'tuna',
+        image: 'https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg'
+    },
+    {
+        name: 'Max',
+        favouriteFood: 'Whiskas',
+        image: 'http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg'
+    },
+    {
+        name: 'Charlie',
+        favouriteFood: 'Sheba',
+        image: 'himalayan_cat.jpg'
+    }
 
-    addCat('Charlie', 'Sheba', 'himalayan_cat.jpg');
+]
+
+const app = function () {
+ catArray.forEach(function (cat, index) {
+     addCat(cat.name, cat.favouriteFood, cat.image)
+ });
+
+
+    // addCat('Charlie', 'Sheba', 'himalayan_cat.jpg');
 
 }
 
@@ -25,7 +53,7 @@ function createImageItem(image) {
 
 function createFoodItem(favouriteFood) {
     const foodItem = document.createElement('li');
-    foodItem.innerText = 'Favourite food: '+ favouriteFood;
+    foodItem.innerText = 'Favourite food: ' + favouriteFood;
     return foodItem;
 }
 
